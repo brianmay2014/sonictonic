6b0342ec5aff00994b7a6623f8a571c233f6a077
Brian May <brianmay2014@gmail.com>
2022-04-29 13:18:47 -0700

    added homepage image, set up theme colors

---
+++ frontend/src/components/HomePage/index.js
@@ -0,0 +1,15 @@
+
+import '../../index.css'
+import './HomePage.css'
+
+
+function HomePage() {
+
+    return (
+        <div className="cover-image">
+            <p id="splash-text">What's next in music is first on SonicTonic</p>
+        </div>
+    );
+}
+
+export default HomePage;
