04668c04400814313823e4c261aea220cc29fd54
Brian May <brianmay2014@gmail.com>
2022-05-02 12:52:53 -0700

    push for debugging

--- frontend/src/components/CabinetDisplay/index.js
+++ frontend/src/components/CabinetDisplay/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SongSquare from '../SongSquare';
+import { useDispatch, useSelector } from 'react-redux';
+import getAllSongs from '../../store/song';
+
 
 function CabinetDisplay() {
 
+    const dispatch = useDispatch();
+    const songList = useSelector((state) => state.song.songs);
+
+    useEffect(() => {
+        dispatch(getAllSongs());
+    }, [dispatch]);
 
     
     return (
         <div className="cabinet-display">
-
+            <p>Hello
+                </p>
+                {/* {songList}; */}
         </div>
     )
 }
+
+export default CabinetDisplay;
--- frontend/src/store/song.js
+++ frontend/src/store/song.js
@@ -1,22 +1,26 @@
-
-
+import { csrfFetch } from './csrf';
 
 const GET_SONG = 'song/GET_SONG';
 const GET_ALL_SONGS = 'song/GET_ALL_SONGS'
 
 //get song with album and genre info
-const getSong = song => ({
-    type: GET_SONG,
-    song
-});
+const getSong = song => {
+    return {
+        payload: song,
+        type: GET_SONG,
+    }
+};
 
-const getSongList = (song) => ({
-	type: GET_ALL_SONGS,
-	song,
-});
+const getSongList = (songs) => {
+    return {
+        type: GET_ALL_SONGS,
+        payload: songs,
+
+    }
+};
 
 export const getOneSong = (id) => async dispatch => {
-    const response = await fetch(`/api/song/${id}`);
+    const response = await csrfFetch(`/api/song/${id}`);
 
     if (response.ok) {
         const song = await response.json();
@@ -24,11 +28,30 @@ export const getOneSong = (id) => async dispatch => {
     }
 };
 
-export const getAllSongs = (id) => async (dispatch) => {
-	const response = await fetch(`/api/song/`);
+export const getAllSongs = () => async (dispatch) => {
+	const response = await csrfFetch(`/api/song/all`);
 
 	if (response.ok) {
 		const songs = await response.json();
 		dispatch(getSongList(songs));
 	}
 };
+
+const initialState = { songs: null };
+
+const songReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case GET_ALL_SONGS:
+            newState = {...state};
+            newState.songs = action.payload;
+            return newState;
+        default:
+            return state;
+    }
+};
+
+
+console.log(getSongList({test: 'test'}));
+
+export default songReducer;
